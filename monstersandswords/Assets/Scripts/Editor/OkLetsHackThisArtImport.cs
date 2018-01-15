3242c16e7a32339ba9891b546e1908512aa2dfb3
ronswansonator <howdoicodeanything@yahoo.com>
2018-01-14 16:49:49 -0800

    hack import script and adding collision and navigation to level

---
+++ monstersandswords/Assets/Scripts/Editor/OkLetsHackThisArtImport.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class OkLetsHackThisArtImport
+{  
+    [MenuItem("Cusotm Tools/Convert Art Prefabs to Real Ones")]
+    public static void ConvertArtPrefabsToRealOnes()
+    {
+        GameObject root = Selection.activeGameObject;
+        if(!root)
+        {
+            Debug.LogError("Hey select a root object first");
+            return;
+        }
+        OkLetsHackThisArtImport hackerer = new OkLetsHackThisArtImport();
+        for (int i = 0; i < root.transform.childCount; ++i)
+        {
+            hackerer.UpgradeGameObjectPrefab(root.transform.GetChild(i).gameObject);
+        }
+    }
+
+    Dictionary<GameObject, GameObject> _prefabMap = new Dictionary<GameObject, GameObject>();
+
+    void UpgradeGameObjectPrefab(GameObject g)
+    {
+        GameObject oldPrefab = PrefabUtility.GetPrefabParent(g as Object) as GameObject;
+        if (oldPrefab)
+        {
+            GameObject prefabToSet = null;
+            bool hasPrefab = _prefabMap.TryGetValue(oldPrefab, out prefabToSet);
+            if (!hasPrefab)
+            {
+                prefabToSet = PrefabUtility.CreatePrefab("Assets/Prefabs/Environment/" + oldPrefab.name + ".prefab", g);
+                _prefabMap.Add(oldPrefab, prefabToSet);
+            }
+
+            TransformExtensions.Transform_RTS rts = g.transform.ExtractRTS();
+            GameObject newG = PrefabUtility.ConnectGameObjectToPrefab(g, prefabToSet);
+            newG.transform.SetFromTransformRTS(rts);
+        }
+        else
+        {
+            for (int i = 0; i < g.transform.childCount; ++i)
+            {
+                this.UpgradeGameObjectPrefab(g.transform.GetChild(i).gameObject);
+            }
+        }
+    }
+}
---
+++ monstersandswords/Assets/Scripts/ExtensionMethods/TransformExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TransformExtensions
+{
+    public struct Transform_RTS
+    {
+        public Vector3 pos;
+        public Quaternion rot;
+        public Vector3 scale;
+    }
+
+    public static Transform_RTS ExtractRTS(this Transform me)
+    {
+        Transform_RTS rts;
+        rts.pos = me.position;
+        rts.rot = me.rotation;
+        rts.scale = me.localScale;
+        return rts;
+    }
+
+    public static void SetFromTransformRTS(this Transform me, Transform_RTS rts)
+    {
+        me.position = rts.pos;
+        me.rotation = rts.rot;
+        me.localScale = rts.scale;
+    }
+}
